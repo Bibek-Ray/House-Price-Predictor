a6c5baff83ceb4afdc0f86399d845d0fd0958a70
Bibek Ray <bibekray.dec@gmail.com>
2023-01-22 15:34:17 +0530

    Updated House Price Predictor

--- MultipleLR_HousingPred.py
+++ MultipleLR_HousingPred.py
@@ -1,29 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+## Importing the libraries
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
-count = 0
+"""## Importing the dataset"""
+
 dataset = pd.read_csv('Housing.csv')
-x = dataset.iloc[: , 1: ].values
-y = dataset.iloc[:, :1].values
+x = dataset.iloc[:, 1:].values
+y = dataset.iloc[:, 0].values
+x1 = dataset.iloc[: , 5:10].values
+x2 = dataset.iloc[: , 11:].values
+x3 = dataset.iloc[: , 10:11].values
+x4 = dataset.iloc[: , 0:5].values
+
+"""# Encoding non numeric data to numeric type"""
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [4, 5, 6, 7, 8, 10, 11])], remainder='passthrough')
-x = np.array(ct.fit_transform(x))
+ct1 = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0, 1])], remainder='passthrough')
+x2 = np.array(ct1.fit_transform(x2))
+ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0, 1, 2, 3, 4])], remainder='passthrough')
+x1 = np.array(ct.fit_transform(x1))
+x5 = np.concatenate((x1, x3), axis=1)
+x6 = np.concatenate((x5, x2), axis=1)
+x7 = np.concatenate((x4, x6), axis=1)
+x= x7
+
+"""## Splitting the dataset into the Training set and Test set"""
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-from sklearn.linear_model import LinearRegression as LR
-regressor = LR()
-regressor.fit(x_train, y_train)
+"""## Training the Random Forest Regression model on the whole dataset"""
 
-y_pred = regressor.predict(x_test)
-np.set_printoptions(precision = 2) #This statement restricts more than 2 values after decimal
-#print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
-for i in range(len(y_test)):
-    if((y_pred[i] <= (1.2 * y_test[i])) and (y_pred[i]) >= (0.8 * y_test[i] )):
-        count += 1
-    Percent = (count/len(y_test)) * 100
-print(Percent)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X_train, y_train)
+
+"""## Predicting the Test set results"""
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+"""## Evaluating the Model Performance"""
+
+from sklearn.metrics import r2_score
+v = r2_score(y_test, y_pred)
+adj_r2 = 1 - (1 - v)*((len(y_pred) - 1)/(len(y_pred) - 13))
+
+print(adj_r2)
